efe050d1f0c9d58c5f11ff1bf6fd1593e7ddc0b0
Tovieye Ozi <tozi@erltelecoms.com>
2020-10-04 17:32:58 +0100

    convert to nextjs+chakra

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,6 @@ import {
   HStack,
   Box,
   Link,
-  createIcon,
   Icon,
   useBreakpointValue,
   Menu,
@@ -14,19 +13,8 @@ import {
   IconButton,
   Text,
 } from "@chakra-ui/core";
-import { RiMenuFill } from "react-icons/ri";
+import { RiMenuFill, RiPlaneFill } from "react-icons/ri";
 import NavItem from "./NavItem";
-
-export const ERLIcon = createIcon({
-  displayName: "ERLIcon",
-  viewBox: "0 0 80 81",
-  path: (
-    <path
-      fill="currentColor"
-      d="M1 41v39h40v-5c0-4.6.2-5 2.3-5 8.1 0 18.9-7.6 23.4-16.4 3.6-6.8 3.8-17.8.5-24.6-4.4-9-15.1-17-22.9-17C41 12 41 12 41 7V2H1v39zm39 .1v29.1l-3.7-.6C11.8 65.2 3 35.9 21.2 19.5c4.8-4.3 11.4-7.3 16.6-7.4L40 12v29.1zM79.5 41c0 21.2.1 29.8.2 19.3.2-10.6.2-28 0-38.5-.1-10.6-.2-2-.2 19.2z"
-    />
-  ),
-});
 
 function ExpandedNav() {
   return (
@@ -84,10 +72,10 @@ function MenuNav() {
     </Menu>
   );
 }
-function ERLLogo() {
+function Logo() {
   const expanded = useBreakpointValue({
-    base: "ERL",
-    sm: "Equinox Resources Limited",
+    base: "RLP",
+    sm: "React Landing Page",
   });
   return (
     <Text textTransform="uppercase" fontWeight="bold" isTruncated>
@@ -110,7 +98,7 @@ export default function Navigation() {
     >
       <Link href="#header">
         <HStack>
-          <ERLIcon boxSize={10} /> <Box as={ERLLogo} flexShrink={3} />
+          <Icon as={RiPlaneFill} boxSize={10} /> <Box as={Logo} flexShrink={3} />
         </HStack>
       </Link>
       {expanded ? <ExpandedNav /> : <MenuNav />}
